6793180d47e608bee06bbe3ed6759d910378fca7
Bsh54 <Shadrakbsh@gmail.com>
2026-02-15 12:55:54 +0100

    Catalogue : Mise en place du registre des modèles de CV disponibles

---
+++ src/data/cvModels.ts
@@ -0,0 +1,5 @@
+import Moderne01 from "@/templates/moderne/Moderne01";
+
+export const cvModels: Record<string, any> = {
+  "1": { name: "Moderne Strategique", component: Moderne01, defaultColor: "#00a99d" },
+};
