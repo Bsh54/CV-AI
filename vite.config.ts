3833548baa55b36260644a396bd894ff8be65da4
Bsh54 <Shadrakbsh@gmail.com>
2026-03-09 21:20:16 +0100

    fix: allow alternative port if 3000 is in use

--- vite.config.ts
+++ vite.config.ts
@@ -8,8 +8,8 @@ import { defineConfig } from "vite"
 export default defineConfig({
   plugins: [react(), tailwindcss()],
    server: {
-    strictPort: true,
-    host: true,     
+    strictPort: false,
+    host: true,
     port: 3000
   },
   resolve: {
